29ed70709c8d8bf7728c804b61b57eaa6302adb5
yilp7 <phantom351559301@gmail.com>
2021-10-22 09:58:51 +0800

    rm build files

--- build-Demo_combined-Desktop_Qt_5_12_10_MSVC2017_32bit-Release/CMakeFiles/Demo_param.dir/icon.i
+++
@@ -1,2 +0,0 @@
-#line 1 "C:\\Users\\2234\\Documents\\workspace\\qt5\\Demo_combined\\icon.rc"
-IDI_ICON1 ICON DISCARDABLE "resources/icon.ico"
--- build-Demo_combined-Desktop_Qt_5_12_10_MinGW_32_bit-Release/CMakeFiles/Demo_param.dir/Demo_param_autogen/3YJK5W5UP7/qrc_resources.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/Demo_param.dir/Demo_param_autogen/3YJK5W5UP7/qrc_resources.cpp.obj: \
- C:\Users\2234\Documents\workspace\qt5\Demo_combined\build-Demo_combined-Desktop_Qt_5_12_10_MinGW_32_bit-Release\Demo_param_autogen\3YJK5W5UP7\qrc_resources.cpp
