48f56638755dca6d3aa1a392a84dad9de3541c8d
yilp7 <phantom351559301@gmail.com>
2021-11-17 14:03:59 +0800

    utils.h

---
+++ utils.h
@@ -0,0 +1,13 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <QtCore>
+#include <QtWidgets>
+#include "opencv2/core.hpp"
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/videoio.hpp>
+
+extern QFont monaco, consolas;
+
+#endif // UTILS_H
